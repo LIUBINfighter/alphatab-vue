202712c62043815d2b58552a019d18fe7e26594a
JayBridge <12310903@mail.sustech.edu.cn>
2025-05-09 16:11:27 +0800

    feat: 新增《天空之城》乐谱文件，包含和弦设置、拍子和加载逻辑；更新 App.vue 以传递文件路径给 SimpleDisplay 组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import SimpleDisplay from './components/SimpleDisplay.vue';
-import SimplePlayer from './components/SimplePlayer.vue'
+import SimplePlayer from './components/SimplePlayer.vue';
 </script>
 
 <template>
   <!-- <SimplePlayer /> -->
-   <SimpleDisplay />
+  <SimpleDisplay :file="'/scores/吉他与孤独与蓝色星球.gpx'" />
 </template>
 
 <style scoped>
