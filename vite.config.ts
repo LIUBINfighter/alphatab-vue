7ee10fce31c4b90df9c419bd6d0d177c5761c71f
JayBridge <12310903@mail.sustech.edu.cn>
2025-05-12 16:18:33 +0800

    添加 GitHub Pages 部署工作流；更新 Vite 配置以动态设置基础 URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// 获取仓库名称作为base URL
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   plugins: [vue()],
 })
