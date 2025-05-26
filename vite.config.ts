974f3a3a9c6d83e575cb5cb038518080c937fad6
JayBridge <12310903@mail.sustech.edu.cn>
2025-05-26 14:36:30 +0800

    hotfix: 404 & url

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,5 @@ export default defineConfig(({ mode }) => { // 注意这里接收 mode 参数
     // - 本地开发 (mode !== 'production') 时，base 通常是 '/'，这样访问 http://localhost:5173/ 即可
     // - 生产构建 (mode === 'production') 时，base 应该是 '/<你的仓库名>/'
     base: mode === 'production' && repoName ? `/${repoName}/` : '/',
-    define: {
-      // 将 BASE_URL 注入到 404.html 等静态文件中 (注意双引号和JSON.stringify)
-      '"{{ VITE_BASE_URL }}"': JSON.stringify(repoName) 
-    }
   };
 });
